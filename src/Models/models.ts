34635358d5dc386e6182af922cfd8a3479fb77c3
Arvind Deshraj <arvind.deshraj@gmail.com>
2019-09-22 21:13:30 +0530

    Created User and Chat Schema.

--- src/Models/models.ts
+++ src/Models/models.ts
@@ -5,7 +5,43 @@ const userSchema = new Schema({
         type: String,
         default: null
     },
+    givenName: {
+    	type: String,
+    	default: null
+    },
+    familyName: {
+    	type: String,
+    	default: null
+    },
+    email: {
+    	type: String,
+    	default:null
+    },
+    pictureUrl: {
+    	type: String,
+    	default: null
+    },
+    chats: [{
+    	type: String,
+    	default: null
+    }]
+});
+
+
+const chatSchema = new Schema({
+	id: {
+		type: String,
+		default: null
+	},
+	messages: [{
+		datatype: {
+			type: String,
+			default: "text"
+		},
+		data: String  
+	}]
 });
 
 let user = model('user', userSchema);
-
+let chat = model('chat', chatSchema);
+export {user, chat};
